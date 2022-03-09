21da95cdd021d557bf2f0f24059bd9c4ee3ef48a
dermo <dermot.logue@dkit.ie>
2022-03-09 10:40:23 +0000

    updates

--- src/main/java/dkit/oop/App.java
+++ src/main/java/dkit/oop/App.java
@@ -24,32 +24,55 @@ public class App {
         menu.add(new RestaurantMenuItem("Steak", 12.99f));
         menu.add(new RestaurantMenuItem("Salad", 5.99f));
 
+        // Initialize an object that has a field called "menu" that is
+        // a List of RestaurantMenuItems. (List embedded in Object)
         RestaurantWithMenu restaurantWithMenu =
                 new RestaurantWithMenu("Future Studio Steak House", menu);
 
         Gson gsonParser = new Gson();
         String restaurantJsonString = gsonParser.toJson(restaurantWithMenu);
+        //note how the list is presented in the output with square brackets
+        //  "{ "name":"Future...", "menu":[ {...."
+
 
         System.out.println("Restaurant object as JSON String: ");
         System.out.println(restaurantJsonString);
 
-        // MenuItems as list only style
+        // MenuItems as list only style.
+        // Sometimes, an API may return simply a list of objects
+        // that are not embedded in any object. The following code demonstrates
+        // this, by serializing the ArrayList into a Json String.
+        // Note that the JSON String starts with a square bracket "["
+        // indicating that it is a List.
+
         List<RestaurantMenuItem> menuList = new ArrayList<>();
         menuList.add(new RestaurantMenuItem("Spaghetti", 7.99f));
         menuList.add(new RestaurantMenuItem("Steak", 12.99f));
         menuList.add(new RestaurantMenuItem("Salad", 5.99f));
+
         gsonParser = new Gson();
-        String restaurantJsonString2 = gsonParser.toJson(menuList);
+        String menuListJson = gsonParser.toJson(menuList);
 
         System.out.println("Restaurant object as JSON String: (list only style)");
-        System.out.println(restaurantJsonString2);
+        System.out.println(menuListJson);
 
         ////////////////////////////////////////////////////////////////////////
         //
         // De-serialization
+        // -  parse a JSON String containing an object that has an
+        //    embedded list (the "menu" field) of objects and generate
+        //    a Java Object with an embedded Array ( Founder[] )
+
+
+        //////////////// Founder sample ////////////////////
+
 
         String founderJson = "[{'name': 'Christian','flowerCount': 1}, {'name': 'Marcus', 'flowerCount': 3}, {'name': 'Norman', 'flowerCount': 2}]";
 
+        // To tell the Gson parser that we are processing a list of objects
+        // that are to be mapped to an Array of Founder objects, we use
+        // the syntax Founder[].class.  This will cause the Gson parser to return
+        // a reference to the newly created and populated Array of Founder objects.
         Founder[] founderArray = gsonParser.fromJson(founderJson, Founder[].class);
 
         System.out.println("Founder array : ");
@@ -72,7 +95,10 @@ public class App {
 
 //TODO  Implement the following Array of RiseTime objects.
 // Populate an ArrayList of RiseTime objects, then
-// Serialize and Deserialize them
+// Serialize and Deserialize them.
+// Note that the following is NOT in Json String Format !
+// so you must edit it to make it a JSON String.
+//
 //  [
 //        {
 //        "duration": 529,
